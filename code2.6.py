a98b4459e4be9c27a2298755b3ebfb50c7408d8c
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-11 11:36:32 +0100

    update 11112020

---
+++ code2.6.py
@@ -0,0 +1,9 @@
+course = "Python Programming"
+print(course.upper())
+print(course)
+print(course.title())
+print(course.rstrip())
+print(course.find("Pro"))
+print(course.replace("P", "J"))
+print("Pro" in course)
+print("swift" not in course)

8e985bde2f5a9426e65270b211930a1b9de86e31
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-12 12:14:11 +0100

    update 12112020

---
+++ code2.8.py
@@ -0,0 +1,16 @@
+import math
+print(round(2.9))
+print(abs(-3.9))
+
+print(math.ceil(2.9))
+
+print(math.copysign(2, 3))
+
+print(math.fabs(2.33))
+
+print(math.factorial(9))
+print(math.floor(2.3))
+
+x = math.floor(2.55)
+
+print(x)
---
+++ code2.9.py
@@ -0,0 +1,3 @@
+x = input("x: ")
+y = (x) + 1
+print(y)

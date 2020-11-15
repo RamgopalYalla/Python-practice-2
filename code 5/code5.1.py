54741fecafc680916fac745df53739bf4713a8f9
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-16 00:38:22 +0100

    update 15112020

---
+++ code 5/code5.1.py
@@ -0,0 +1,10 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
+print(combined)
+print(numbers)
+chars = list("Hello world")
+print(chars)
+print(len(chars))

6bd44d3f35fbdca370902f839c25d5b98a35b8a4
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-18 23:58:44 +0100

    update 18112020

--- code 6/code6.3.py
+++ code 6/code6.3.py
@@ -12,5 +12,18 @@ try:
 except ValueError as error:
     pass
 """
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+
+
+ sfactor = calculate_xfactor(-1)
+ if xfactor == None:
+     pass
+
+"""
 
 print(timeit(code1, number=10000))

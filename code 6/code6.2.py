450257cc50370866500ba13f70a13b7ff9ec188b
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-18 22:52:03 +0100

    update 18112020

--- code 6/code6.2.py
+++ code 6/code6.2.py
@@ -1,7 +1,11 @@
 try:
+    file = open("app.py")
     age = int(input("age: "))
     xfactor = 10 / age
+    file.close()
 except (ValueError, ZeroDivisionError):
     print("Please enter correct age")
 else:
     print("No exceptions were thrown")
+finally:
+    file.close()

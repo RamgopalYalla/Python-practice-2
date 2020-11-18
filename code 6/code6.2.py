a47e63049b97dfe90296b54ef4f54b3b057729a6
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-18 22:46:06 +0100

    update 18112020

--- code 6/code6.2.py
+++ code 6/code6.2.py
@@ -1,7 +1,7 @@
 try:
     age = int(input("age: "))
     xfactor = 10 / age
-except ValueError:
+except (ValueError, ZeroDivisionError):
     print("Please enter correct age")
 else:
     print("No exceptions were thrown")

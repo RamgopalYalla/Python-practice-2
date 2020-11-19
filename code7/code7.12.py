78d13bed6220c69a0098eac7f0e0d6d007f16837
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-19 16:35:48 +0100

    update 19112020

---
+++ code7/code7.12.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+class Mammel(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammel()
+print(isinstance(m, object))
+issubclass(Mammel , object)

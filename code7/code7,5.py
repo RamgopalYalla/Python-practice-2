f7539dff238ed133d1969707326de7a49d9112b4
Ramgopal Yalla <67060501+RamgopalYalla@users.noreply.github.com>
2020-11-19 15:40:18 +0100

    update 19112020

--- code7/code7,5.py
+++ code7/code7,5.py
@@ -1,22 +1,14 @@
 class Point:
-    default_color = "red"
-
     def __init__(self, x, y):
         self.x = x
-        self.y = y
+        self.y = y#
 
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
 
 
-Point.default_color = "yellow"
-
-point = Point(1, 2)
-print(point.default_color)
-print(point.default_color)
+point = Point(0, 0)
 point.draw()
-
-another = Point(3, 4)
-print(point.default_color)
-
-another.draw()
--- code7/code7.2.py
+++ code7/code7.2.py
@@ -3,9 +3,16 @@ class Point:
         self.x = x
         self.y = y
 
-    def draw(self):
-        print(f"Point ({self.x}, {self.y})")
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
+    def __add__(self, other):
+        return self.x + other.x and self.y + other.y
 
 
-point = Point(1, 2)
-point.draw()
+point = Point(10, 5)
+other = Point(1, 3)
+print(point + other)
